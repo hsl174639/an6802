e67a5b9c213bd216aa0e4c0054f26da85c20f85e
hsl174639 <156890177+hsl174639@users.noreply.github.com>
2026-03-13 11:47:06 +0000

    12132

--- app.py
+++ app.py
@@ -1,25 +1,47 @@
 from flask import Flask, render_template, request
+import joblib
+
+model = joblib.load("foodexp.pkl")
 
 app = Flask(__name__)
+
 @app.route("/",methods=["get","post"])
 def index():
-        return(render_template("index.html"))
+    return(render_template("index.html"))
+
 @app.route("/main",methods=["get","post"])
 def main():
-        return(render_template("main.html"))
+    return(render_template("main.html"))
 
 @app.route("/ethics",methods=["get","post"])
 def ethics():
-        return(render_template("ethics.html"))
+    return(render_template("ethics.html"))
 
 @app.route("/correct",methods=["get","post"])
 def correct():
-        return(render_template("correct.html"))
+    return(render_template("correct.html"))
 
 @app.route("/wrong",methods=["get","post"])
 def wrong():
-        return(render_template("wrong.html"))
+    return(render_template("wrong.html"))
 
+@app.route("/econ",methods=["get","post"])
+def econ():
+    return(render_template("econ.html"))
 
-if __name__=="__main__":
-        app.run()
+@app.route("/foodexp",methods=["get","post"])
+def foodexp():
+    q = float(request.form.get("q"))
+    r = model.predict([[q]])
+    return(render_template("foodexp.html",r=r[0][0]))
+
+@app.route("/chatbot",methods=["get","post"])
+def chatbot():
+    return(render_template("chatbot.html"))
+
+@app.route("/generalQuestion",methods=["get","post"])
+def generalQuestion():
+    return(render_template("generalQuestion.html"))
+
+if __name__ == "__main__":
+    app.run()
